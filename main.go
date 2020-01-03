2df4509985cc783ae9bbae60250052375c0ab04e
Lin Trieu <linnatrieu@gmail.com>
2020-01-03 14:21:26 +0000

    Creates a Dummy Database with a event struct slice

--- main.go
+++ main.go
@@ -16,5 +16,20 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
+type event struct {
+	ID          string `json:"ID"`
+	Description string `json:"Description"`
+	Title       string `json:"Title"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works",
+	},
 }
